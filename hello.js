1370e682b31db5a266b1f9c3f9f93768c992c49a
Yannan23 <yannansun0723@gmail.com>
2025-04-18 09:38:29 +1000

    create object in ts file

--- hello.js
+++
@@ -1,5 +0,0 @@
-console.log("hello");
-var sum = function (a, b) {
-    return a + b;
-};
-console.log(sum(4, 6));
--- hello.ts
+++ hello.ts
@@ -1,7 +1,10 @@
-console.log("hello");
+// console.log("hello");
 
-const sum = (a: number, b: number): number => {
-  return a + b;
-};
+// const sum = (a: number, b: number): number => {
+//   return a + b;
+// };
 
-console.log(sum(4, 6));
+// console.log(sum(4, 6));
+
+let o: { a: string; b?: string; [proName: string]: any };
+o = { a: "hello", b: "Bob", c: "dfd" };
