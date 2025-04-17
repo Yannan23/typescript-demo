9f8ff8d63e35be38f5a20a89008a04c09695f91a
Yannan23 <yannansun0723@gmail.com>
2025-04-17 13:55:41 +1000

    sum function created

---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("hello");
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(4, 6));
---
+++ hello.ts
@@ -0,0 +1,7 @@
+console.log("hello");
+
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(sum(4, 6));
