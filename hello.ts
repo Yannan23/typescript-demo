ff68fa2978c47bfcae2cdb3c412b505b6d4370da
Yannan23 <yannansun0723@gmail.com>
2025-04-18 10:06:16 +1000

    create enum in ts file

--- hello.ts
+++ hello.ts
@@ -10,12 +10,28 @@
 let o: { a: string; b?: string; [propName: string]: any };
 o = { a: "hello", b: "Bob", c: "dfd" };
 
-//array
-let a: Array<number>;
-a = [2, 3];
-
 //function
 let f: (a: number, b: number) => number;
 f = function (n1, n2): number {
   return n1 + n2;
 };
+
+//array
+let a: Array<number>;
+a = [2, 3];
+
+let e: string[]; // let e:Array<string>
+e = ["2", "2"];
+
+let h: [string, string]; //元组，固定长度的数组
+h = ["1", "2"];
+
+//enum 枚举
+
+enum Gender {
+  Male,
+  Female,
+}
+
+let person: { name: string; gender: Gender };
+person = { name: "Emily", gender: Gender.Female };
